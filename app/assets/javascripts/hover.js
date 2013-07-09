a57fca9af9037d470193fa34b2c167fc4018bd6a
sanychcz <alexander@420on.cz>
2013-07-09 16:08:38 +0200

    jquery changing images

---
+++ app/assets/javascripts/hover.js
@@ -0,0 +1,6 @@
+$(function(){
+     $(".main-image").hover(
+          function(){this.src = this.src.replace("off","on");},
+          function(){this.src = this.src.replace("on","off");
+     });
+});
