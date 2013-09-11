9428c8884fc451ac4312cfa85a0f447bb213d4b0
sanychcz <alexander@420on.cz>
2013-09-11 17:56:23 +0200

    changed deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,35 +1,21 @@
 require "rvm/capistrano"
 require "bundler/capistrano"
 
-set :deploy_to, "/home/appman/creations"
+set :application, "creations.cz"
+set :deploy_to, "/home/#{application}"
 set :repository,  "git@github.com:sanychcz/creations.git"
 set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
 set :use_sudo, false
 set :deploy_via, :copy
 
-set :user, "appman"
+set :user, "rails"
 
-role :web, "37.139.4.91"                          # Your HTTP server, Apache/etc
-role :app, "37.139.4.91"                          # This may be the same as your `Web` server
-role :db,  "37.139.4.91", :primary => true # This is where Rails migrations will run
 
-after "deploy:restart", "deploy:cleanup"
-after "deploy", "rvm:trust_rvmrc"
+role :web, application                          # Your HTTP server, Apache/etc
+role :app, application                          # This may be the same as your `Web` server
+role :db,  application, :primary => true # This is where Rails migrations will run
 
-namespace :deploy do
-  task :start do ; end
-  task :stop do ; end
-  task :restart, :roles => :appman, :except => { :no_release => true } do
-    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-  end
-end
-
-namespace :rvm do
-  task :trust_rvmrc do
-    run "rvm rvmrc trust #{release_path}"
-  end
-end
 
 # if you want to clean up old releases on each deploy uncomment this:
 # after "deploy:restart", "deploy:cleanup"
